2381e3ddc0c8deb50642c7d12d67c7d698d12d1c
thilllon <thilllon970@gmail.com>
2024-10-01 20:29:22 +0900

    feat: narrow down `ResilienceEvents` interface (#580)

--- src/interfaces/resilience-events.interface.ts
+++ src/interfaces/resilience-events.interface.ts
@@ -2,8 +2,6 @@ import { ResilienceEventType } from '../enum';
 import { BaseCommand } from '../commands';
 
 export interface ResilienceEvents {
-	[key: string | symbol]: any[];
-
 	[ResilienceEventType.Emit]: [command: BaseCommand];
 	[ResilienceEventType.Success]: [command: BaseCommand];
 	[ResilienceEventType.Failure]: [command: BaseCommand];
